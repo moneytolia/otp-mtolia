597c57370027e3be454f9861183bbe9073c5335f
Gizem <gizem.ay@moneytolia.com>
2023-05-11 12:08:41 +0300

    edit 2

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import styles from '../src/index.module.scss'
 
 interface IProps {
   inputCount: number
-  onChangedOtp: (type1: string) => string
+  onChangedOtp: Function
   inputClassName?: string
 }
 
