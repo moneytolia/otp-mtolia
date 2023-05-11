a30b7d2f0cd30d5a09b2dfbf64444c9f9f140696
Gizem <gizem.ay@moneytolia.com>
2023-05-11 10:31:51 +0300

    first commit

---
+++ src/otpInput.tsx
@@ -0,0 +1,79 @@
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
+import styles from '../otp-input/otpInput.module.scss'
+
+interface IProps {
+  inputCount: number
+  onChangedOtp: (type1: string) => string
+  inputClassName?: string
+}
+
+let currentOTPIndex = 0
+const backSpace = 'Backspace'
+
+export const onlyNumberControl = (value: any) => {
+  return value.replace(/\D/g, '')
+}
+
+const OtpInput = ({ inputCount, onChangedOtp, inputClassName }: IProps) => {
+  const [otp, setOtp] = useState<string[]>(new Array(inputCount).fill(''))
+  const [activeOTPIndex, setActiveOTPIndex] = useState<number>(0)
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleOnChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    const { value } = target
+    const newOTP: string[] = [...otp]
+
+    if (value.length > 2) {
+      const maxIndex = inputCount - 1
+      const valueArr = onlyNumberControl(value).split('')
+      valueArr.forEach((val: string, index: number) => {
+        if (index <= maxIndex) {
+          newOTP[index] = val
+        }
+      })
+      currentOTPIndex = valueArr.length >= maxIndex ? inputCount : valueArr.length
+      setActiveOTPIndex(currentOTPIndex - 1)
+    } else {
+      newOTP[currentOTPIndex] = value.substring(value.length - 1)
+      if (!value) setActiveOTPIndex(currentOTPIndex - 1)
+      else setActiveOTPIndex(currentOTPIndex + 1)
+    }
+    setOtp(newOTP)
+    onChangedOtp(newOTP.join(''))
+  }
+
+  const handleOnKeyDown = ({ key }: React.KeyboardEvent<HTMLInputElement>, index: number) => {
+    currentOTPIndex = index
+    setTimeout(() => {
+      if (key === backSpace) setActiveOTPIndex(currentOTPIndex - 1)
+    }, 10)
+  }
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [activeOTPIndex])
+
+  return (
+    <div className={styles.default}>
+      {otp.map((_item, index) => {
+        return (
+          <React.Fragment key={index}>
+            <input
+              ref={index == activeOTPIndex ? inputRef : null}
+              type='number'
+              inputMode='numeric'
+              className={inputClassName || styles.otp}
+              onChange={handleOnChange}
+              onKeyDown={(e) => handleOnKeyDown(e, index)}
+              value={otp[index]}
+              autoComplete='one-time-code'
+            />
+            {index === otp.length - 1 ? null : <span />}
+          </React.Fragment>
+        )
+      })}
+    </div>
+  )
+}
+
+export default OtpInput
